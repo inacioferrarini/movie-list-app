2487efaa1e31ad9a577dd4f423fb925d21a07e21
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2019-09-07 12:05:14 -0300

    Updates project

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'fastlane', '~> 2.120.0'
+gem 'cocoapods', '~> 1.7.4'
+gem 'slather', '~> 2.4.7'
---
+++ MovieList/MovieList/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  MovieList
+//
+//  Created by José Inácio Athayde Ferrarini on 07/09/19.
+//  Copyright © 2019 José Inácio Athayde Ferrarini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}

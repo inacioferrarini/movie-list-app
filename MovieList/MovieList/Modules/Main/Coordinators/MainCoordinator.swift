146bdffcaa5745331873d0b9ec06d8fc06ff3a0f
Inácio Ferrarini <inacio.ferrarini@gmail.com>
2019-09-07 13:48:40 -0300

    Updates to initialize submodules

--- MovieList/MovieList/Modules/Main/Coordinators/MainCoordinator.swift
+++ MovieList/MovieList/Modules/Main/Coordinators/MainCoordinator.swift
@@ -1,14 +1,6 @@
-//
-//  MainCoordinator.swift
-//  MovieList
-//
-//  Created by José Inácio Athayde Ferrarini on 07/09/19.
-//  Copyright © 2019 José Inácio Athayde Ferrarini. All rights reserved.
-//
-
 import Foundation
 import Common
-//import MovieList
+import MovieCatalog
 import Favorites
 
 class MainCoordinator: NSObject, Coordinator {
@@ -25,13 +17,15 @@ class MainCoordinator: NSObject, Coordinator {
     }
 
     internal func loadModules() {
-//        let modules: [Module] = [
-//            StoreFrontModule(),
-//            LoginModule()
-//        ]
-//
-//        self.tabBarController.viewControllers = modules.map({ return $0.viewController })
-//        print("loading modules ...")
+        let modules: [Module] = [
+            MovieCatalogModule(tabBar: tabBarController),
+            FavoritesModule(tabBar: tabBarController)
+        ]
+
+        for module in modules {
+            module.coordinator.start()
+        }
+
     }
 
 }
